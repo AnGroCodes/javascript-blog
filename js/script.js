9a7bb1620cdea4ff6a770f444d3400ee75f2415e
Anna Grochowska <anna.grochowska67@gmail.com>
2021-02-08 18:21:41 +0000

    Fix display articles after click

--- js/script.js
+++ js/script.js
@@ -1,88 +1,61 @@
 'use strict';
 
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
 function titleClickHandler(event){
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
-  console.log(event);
-
-  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
+    activeLink.classList.remove('active');
   }
 
-  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
-  console.log('clickedElement:', clickedElement);
-
-  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
+    activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
-  console.log(articleSelector);
   
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
   const targetArticle = document.querySelector(articleSelector);
-  console.log(targetArticle);
-
-  /* add class 'active' to the correct article */
 
   targetArticle.classList.add('active');
 }
 
-  const links = document.querySelectorAll('.titles a');
-
-  for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-  }
-
-  const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
-
 function generateTitleLinks(){
-  const clickedElement = this;
 
-  /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
-  /* find all the articles and save them to variable: articles */
   const articles = document.querySelectorAll(optArticleSelector);
 
   let html = '';
 
-  /* for each article */
   for(let article of articles){
   
-  /* get the article id */
-  const articleId = clickedElement.getAttribute('id');
+    const articleId = article.getAttribute('id');
 
-  /* find the title element */
-  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-  /* get the title from the title element */
-  articleTitle = clickedElement.getAttribute('post-title');
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-  /* create HTML of the link */
-  const linkHTML = '<li><a href="#"' + articleId + '><span>' + articleTitle + '</span></a></li>';
-  console.log(linkHTML);
-
-  /* insert link into html variable*/
-  let html = html + linkHTML;
+    html = html + linkHTML;
   }
+  
   titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
 
 generateTitleLinks();
